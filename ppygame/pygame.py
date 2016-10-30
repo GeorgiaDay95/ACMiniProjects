44136b0b77632628b65c0a1b2438795c9267eaf9
Georgia Day <georgiaday@Georgias-MacBook.local>
2016-10-30 18:02:00 +0000

    further tweeks

--- ppygame/pygame.py
+++ ppygame/pygame.py
@@ -1,7 +1,7 @@
 from graphics import *
 
 def main():
-    window = GraphWin("my window", 850, 500)
+    window = GraphWin("my window", 900, 600)
     
     myList = []
     textFile = open("data.txt")
@@ -16,42 +16,74 @@ def main():
     directionArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     linesDrawn = []
+    shapesDrawn = []
     
 #    line2 = Line(Point(20,500), Point(20,100))
 #    line2.draw(window)
     
+    
+    
+    
     x = 30
     xOffset = 30
     y = 20
-    bottomY = 500
+    bottomY = 600
     minY = 20
+
     
     for i in range (len(numberData)):
         line1 = Line(Point(x,bottomY), Point(x,bottomY-y))
         line1.setFill("green")
         line1.draw(window)
         linesDrawn.append(line1)
+        movingCircle = Circle(Point(x,bottomY-y), 4.5)
+        movingCircle.setFill("white")
+        movingCircle.setOutline("white")
+        movingCircle.draw(window)
+        shapesDrawn.append(movingCircle)
+        
+        myText = Text(Point(x, 40), str(numberData[i]))
+        myText.setTextColor("black")
+        myText.setSize(15)
+        myText.draw(window)
+        
         x = x + xOffset
+    
+        
     y = 20
     while True:
         x = 30
-        xOffset = 30
-        bottomY = 500
         for i in range (len(numberData)):
             y = yDataArray[i]
             maxY = int(numberData[i])*6
 #            print directionArray[i]
+            if ( y > maxY):
+                y = maxY
+                directionArray[i] = 1
+                shapesDrawn[i].setFill("black")
+                shapesDrawn[i].move(0,-maxY)
+            if ( y < minY):
+                y = minY
+                directionArray[i] = 0
+                moveY = 10
+                howFar = 0
+                while howFar < maxY:
+                    howFar = howFar + 10
+                    shapesDrawn[i].setFill("black")
+                    shapesDrawn[i].move(0, moveY)
+                shapesDrawn[i].setFill("white")
+            
+            
+            
+            
+            
             if (directionArray[i] == 0):
                 yDataArray[i] = yDataArray[i] + 10
                 colour = "green"
             if (directionArray[i] == 1):
                 yDataArray[i] = yDataArray[i] - 10
                 colour = "red"
-                   
-            if ( y >= maxY):
-                directionArray[i] = 1
-            if ( y <= minY):
-                directionArray[i] = 0
+           
 
         
 #            print y, maxY, directionArray[i]
@@ -62,31 +94,9 @@ def main():
             linesDrawn[i] = line1
             x = x + xOffset
 
-#    
-#    
-#    keyPressedG = True
-#    while (keyPressedG == True):
-#        line1 = Line(Point(20,500), Point(20,200))
-#        line1.draw(window)
-#        time.sleep(3000)
-#        line1 = Line(Point(20,500), Point(20,400))
-#        line1.draw(window)
-#        
-#    
-    
+            
     window.getMouse() # Pause to view result
     window.close()    # Close window when done
 
 main()
-
-
-    
-#    for results in fileContent:
-#        myList.append(int(results))
-#        
-#    textFile.close()
-#    myResults = Text(Point(425, 470), myList)
-#    myResults.setSize(20)
-#    myResults.draw(window)
-#    
-#    
+   
